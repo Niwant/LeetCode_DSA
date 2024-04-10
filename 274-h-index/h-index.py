15bcb85ce481029a2646b13a03c8317ee370404e
Niwant Salunke <69031168+Niwant@users.noreply.github.com>
2024-04-10 14:59:01 +0530

    Time: 43 ms (45.34%) | Memory: 16.8 MB (78.86%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n=len(citations)
+        citations.sort()
+
+        for i in range(n):
+            if n-i <= citations[i]:
+                return n-i
+        return 0
